15dc99c521fd5f6bea79489daf4d1ac74af542b7
David Hülsberg <david.huelsberg@outlook.de>
2021-11-28 23:47:38 +0100

    CSS-Datei eingebunden.

--- index.php
+++ index.php
@@ -5,16 +5,17 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>TestKA</title>
+    <link rel="stylesheet" href="./css/stylesheet.css">
 </head>
 <body>
-    <div>
-        <div>
+    <div class="layout wrapper">
+        <div class="layout oben">
             <p>Blau</p>
         </div>
-        <div>
+        <div class="layout links">
             <p>Gelb</p>
         </div>
-        <div>
+        <div class="layout rechts">
             <p>Weiß</p>
         </div>
     </div>
