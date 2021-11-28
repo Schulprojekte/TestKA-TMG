075207e1e2c0cd863e28bac7144c3235e91ca436
David Hülsberg <david.huelsberg@outlook.de>
2021-11-29 00:11:05 +0100

    PHP-Code zum auslesen der Datenbank erstellt.

--- anzeige.php
+++ anzeige.php
@@ -7,6 +7,19 @@
     <title>DB-Daten</title>
 </head>
 <body>
-    
+    <?php
+        include('zugriff.inc');
+        $connection = mysqli_connect($host, $user, $password, $database) or die("Verbindung zum Server konnte nicht hergestellt werden!");
+        $query = "SELECT name, klasse, beschreibung FROM daten ORDER BY name desc;";
+        echo "<table border='1'>";
+        echo "<tr><th>Name:</th><th>Klasse:</th><th>Beschreibung:</th>";
+        if($result = $connection->query($query)) {
+            while($row = $result->fetch_assoc()) {
+                echo "<tr><td>".$row["name"]."</td><td>".$row["klasse"]."</td><td>".$row["beschreibung"]."</td></tr>";
+            }
+        }
+        echo "</table>";
+        mysqli_close($connection);
+    ?>
 </body>
 </html>
